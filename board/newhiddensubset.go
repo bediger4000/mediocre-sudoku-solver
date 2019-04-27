327c24aa52fdbaa0901188200d56252b9b1e2a07
Bruce Ediger <bediger@stratigery.com>
2019-04-27 14:42:28 -0600

    Make it work right. Rows, columns, blocks

--- board/newhiddensubset.go
+++ board/newhiddensubset.go
@@ -3,7 +3,7 @@ package board
 import "fmt"
 
 func (bd *Board) HiddenPair(announceSolutions bool) int {
-	found := 0
+	eliminated := 0
 	for colNo, column := range Columns {
 		digitMap := make(map[int][]CellCoord)
 		for _, coord := range column {
@@ -13,10 +13,8 @@ func (bd *Board) HiddenPair(announceSolutions bool) int {
 				}
 			}
 		}
-		// fmt.Printf("Column %d:\n", colNo)
 		pairCells := make(map[int][]CellCoord)
 		for digit, cells := range digitMap {
-			// fmt.Printf("\t%d %v\n", digit, cells)
 			if len(cells) == 2 {
 				pairCells[digit] = cells
 			}
@@ -38,20 +36,121 @@ func (bd *Board) HiddenPair(announceSolutions bool) int {
 						}
 						pairedAlready[digit] = true
 						pairedAlready[otherDigit] = true
-						found++
-						cleanHiddenPair(bd, digit, otherDigit, cells)
+						eliminated += cleanHiddenPair(bd, digit, otherDigit, cells)
 					}
 				}
 			}
 		}
 	}
-	/*
-		for _, row := range Rows {
-			for _, coord := range row {
+	for rowNo, row := range Rows {
+		digitMap := make(map[int][]CellCoord)
+		for _, coord := range row {
+			if !bd[coord.X][coord.Y].Solved {
+				for _, digit := range bd[coord.X][coord.Y].Possible {
+					digitMap[digit] = append(digitMap[digit], coord)
+				}
 			}
 		}
-	*/
-	return found
+		pairCells := make(map[int][]CellCoord)
+		for digit, cells := range digitMap {
+			if len(cells) == 2 {
+				pairCells[digit] = cells
+			}
+		}
+		if len(pairCells) >= 2 {
+			pairedAlready := make(map[int]bool)
+			for digit, cells := range pairCells {
+				for otherDigit, otherCells := range pairCells {
+					if digit == otherDigit {
+						continue
+					}
+					if pairedAlready[digit] || pairedAlready[otherDigit] {
+						continue
+					}
+					if pairOfCellsEqual(cells, otherCells) {
+						if announceSolutions {
+							fmt.Printf("Row %d: %d & %d a hidden pair in %v and %v\n",
+								rowNo, digit, otherDigit, cells, otherCells)
+						}
+						pairedAlready[digit] = true
+						pairedAlready[otherDigit] = true
+						eliminated += cleanHiddenPair(bd, digit, otherDigit, cells)
+					}
+				}
+			}
+		}
+	}
+	for blockNo, block := range Blocks {
+		digitMap := make(map[int][]CellCoord)
+		for _, coord := range block {
+			if !bd[coord.X][coord.Y].Solved {
+				for _, digit := range bd[coord.X][coord.Y].Possible {
+					digitMap[digit] = append(digitMap[digit], coord)
+				}
+			}
+		}
+		pairCells := make(map[int][]CellCoord)
+		for digit, cells := range digitMap {
+			if len(cells) == 2 {
+				pairCells[digit] = cells
+			}
+		}
+		if len(pairCells) >= 2 {
+			pairedAlready := make(map[int]bool)
+			for digit, cells := range pairCells {
+				for otherDigit, otherCells := range pairCells {
+					if digit == otherDigit {
+						continue
+					}
+					if pairedAlready[digit] || pairedAlready[otherDigit] {
+						continue
+					}
+					if pairOfCellsEqual(cells, otherCells) {
+						if announceSolutions {
+							fmt.Printf("Block %d: %d & %d a hidden pair in %v and %v\n",
+								blockNo, digit, otherDigit, cells, otherCells)
+						}
+						pairedAlready[digit] = true
+						pairedAlready[otherDigit] = true
+						eliminated += cleanHiddenPair(bd, digit, otherDigit, cells)
+						// For the block case, it's possible to eliminate the
+						// digits of the hidden pair from other cells,
+						// if the pair shows up in the same row or column.
+						if cells[0].X == cells[1].X {
+							// same row
+							row := Rows[cells[0].X]
+							for _, cell := range row {
+								if bd[cell.X][cell.Y].Solved {
+									continue
+								}
+								if (cell.X == cells[0].X && cell.Y == cells[0].Y) ||
+									(cell.X == cells[1].X && cell.Y == cells[1].Y) {
+									continue
+								}
+								eliminated += bd.SpliceOut(cell.X, cell.Y, digit)
+								eliminated += bd.SpliceOut(cell.X, cell.Y, otherDigit)
+							}
+						} else if cells[0].Y == cells[1].Y {
+							// same column
+							col := Columns[cells[0].Y]
+							for _, cell := range col {
+								if bd[cell.X][cell.Y].Solved {
+									continue
+								}
+								if cell.X == cells[0].X && cell.Y == cells[0].Y ||
+									cell.X == cells[1].X && cell.Y == cells[1].Y {
+									continue
+								}
+								eliminated += bd.SpliceOut(cell.X, cell.Y, digit)
+								eliminated += bd.SpliceOut(cell.X, cell.Y, otherDigit)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return eliminated
 }
 
 // It says []CellCoord, but each of them is length 2
@@ -69,12 +168,16 @@ func cellsEqual(a, b CellCoord) bool {
 	return a.X == b.X && a.Y == b.Y
 }
 
-func cleanHiddenPair(bd *Board, digit int, otherDigit int, cells []CellCoord) {
+func cleanHiddenPair(bd *Board, digit int, otherDigit int, cells []CellCoord) int {
+	eliminated := 0
 	for _, cell := range cells {
-		for _, possibleDigit := range bd[cell.X][cell.Y].Possible {
-			if possibleDigit != digit && possibleDigit != otherDigit {
-				bd.SpliceOut(cell.X, cell.Y, possibleDigit)
+		maybeDigits := make([]int, len(bd[cell.X][cell.Y].Possible))
+		copy(maybeDigits, bd[cell.X][cell.Y].Possible)
+		for _, possibleDigit := range maybeDigits {
+			if (possibleDigit != digit) && possibleDigit != otherDigit {
+				eliminated += bd.SpliceOut(cell.X, cell.Y, possibleDigit)
 			}
 		}
 	}
+	return eliminated
 }
