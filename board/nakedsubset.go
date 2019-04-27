854323f12aa9448b3101ff5b2bc31971630331f4
Bruce Ediger <bediger@stratigery.com>
2019-04-27 09:13:55 -0600

    Get it to kind of work

--- board/nakedsubset.go
+++ board/nakedsubset.go
@@ -1,6 +1,8 @@
 package board
 
-func (bd *Board) NakedSubset() {
+func (bd *Board) NakedSubset() bool {
+
+	var foundSome bool
 
 	for row := 0; row < 9; row++ {
 		twoCounts := 0
@@ -34,8 +36,8 @@ func (bd *Board) NakedSubset() {
 						continue
 					}
 					// Splice either of the two digits out of bd[row][c].Possible
-					bd.SpliceOut(row, c, bd[row][twoCols[0]].Possible[0])
-					bd.SpliceOut(row, c, bd[row][twoCols[0]].Possible[1])
+					foundSome = foundSome || bd.SpliceOut(row, c, bd[row][twoCols[0]].Possible[0])
+					foundSome = foundSome || bd.SpliceOut(row, c, bd[row][twoCols[0]].Possible[1])
 				}
 			}
 		}
@@ -73,10 +75,57 @@ func (bd *Board) NakedSubset() {
 						continue
 					}
 					// Splice either of the two digits out of bd[row][c].Possible
-					bd.SpliceOut(r, col, bd[twoRows[0]][col].Possible[0])
-					bd.SpliceOut(r, col, bd[twoRows[0]][col].Possible[1])
+					foundSome = foundSome || bd.SpliceOut(r, col, bd[twoRows[0]][col].Possible[0])
+					foundSome = foundSome || bd.SpliceOut(r, col, bd[twoRows[0]][col].Possible[1])
 				}
 			}
 		}
 	}
+	for _, block := range Blocks {
+		twoCounts := 0
+		for _, cell := range block {
+			if bd[cell.X][cell.Y].Solved {
+				continue
+			}
+			if len(bd[cell.X][cell.Y].Possible) == 2 {
+				twoCounts++
+			}
+		}
+		if twoCounts != 2 {
+			continue
+		}
+		twoCells := []CellCoord{}
+		for _, cell := range block {
+			if bd[cell.X][cell.Y].Solved {
+				continue
+			}
+			if len(bd[cell.X][cell.Y].Possible) == 2 {
+				twoCells = append(twoCells, cell)
+			}
+		}
+
+		X0, Y0 := twoCells[0].X, twoCells[0].Y
+		X1, Y1 := twoCells[1].X, twoCells[1].Y
+
+		if bd[X0][Y0].Possible[0] == bd[X1][Y1].Possible[0] {
+			if bd[X0][Y0].Possible[1] == bd[X1][Y1].Possible[1] {
+				for _, cell := range block {
+					if bd[cell.X][cell.Y].Solved {
+						continue
+					}
+					if cell.X == X0 && cell.Y == Y0 {
+						continue
+					}
+					if cell.X == X1 && cell.Y == Y1 {
+						continue
+					}
+					// Splice either of the two digits out of bd[row][c].Possible
+					foundSome = foundSome || bd.SpliceOut(cell.X, cell.Y, bd[X0][Y0].Possible[0])
+					foundSome = foundSome || bd.SpliceOut(cell.X, cell.Y, bd[X0][Y0].Possible[1])
+				}
+			}
+		}
+	}
+
+	return foundSome
 }
