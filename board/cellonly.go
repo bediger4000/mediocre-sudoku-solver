d7ab71a9ee6566cbadbd535cb21a6613ad5846cf
Bruce Ediger <Bruce_Ediger@comcast.com>
2019-05-01 11:04:20 -0600

    Use new unified types to do rows, cols and blocks

--- board/cellonly.go
+++ board/cellonly.go
@@ -2,24 +2,27 @@ package board
 
 import "fmt"
 
+var buddyThing []ThingType = []ThingType{RowThing, ColumnThing, BlockThing}
+
 func (bd *Board) BlockOnly(announceSolution bool) int {
 	found := 0
-	for blockno, block := range Blocks {
-		digitCount := bd.CountPossibleDigits(BlockThing, blockno)
-
-		for _, cell := range block {
-			row, col := cell.X, cell.Y
-			if bd[row][col].Solved {
-				continue
-			}
-			for _, possibleDigit := range bd[row][col].Possible {
-				if digitCount[possibleDigit] == 1 {
-					if announceSolution {
-						fmt.Printf("Mark <%d,%d> solved with %d, only possible digit for block\n", row, col, possibleDigit)
+	for neighborhoodNo, neighborhood := range Neighborhoods {
+		for buddiesNo, buddies := range neighborhood {
+			digitCount := bd.CountPossibleDigits(buddyThing[neighborhoodNo], buddiesNo)
+			for _, cell := range buddies {
+				row, col := cell.X, cell.Y
+				if bd[row][col].Solved {
+					continue
+				}
+				for _, possibleDigit := range bd[row][col].Possible {
+					if digitCount[possibleDigit] == 1 {
+						if announceSolution {
+							fmt.Printf("Mark <%d,%d> solved with %d, only possible digit for block\n", row, col, possibleDigit)
+						}
+						bd.MarkSolved(row, col, possibleDigit)
+						found++
+						break
 					}
-					bd.MarkSolved(row, col, possibleDigit)
-					found++
-					break
 				}
 			}
 		}
